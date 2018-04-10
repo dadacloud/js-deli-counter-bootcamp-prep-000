5ee59166cbc260aa63e94a3426b35065b963ff54
Marc Jean-Gilles <marcjeangilles@gmail.com>
2018-04-10 15:02:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var katzDeli = [];
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine){
-  var n = 1;
+  var n = 1; n ++;
   katzDeliLine.push(n);//add new person to line, give them number, keep track of numbers throughout day, and when new ppl come in without anyone else in line, the number keeps rising
-  return `You are number ` + katzDeliLine.length;
+  return `You are number ` + katzDeliLine;
 }
 
 
